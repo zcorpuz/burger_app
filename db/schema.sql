9d0b6f1bebbe5513f67d55122135214ff4871c8b
Zach Corpuz <zachcorpuz@yahoo.com>
2020-07-06 21:16:43 -0700

    Refactored database

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,8 @@ CREATE DATABASE burgers_db;
 
 USE burgers_db;
 CREATE TABLE burgers (
-    id AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(50) NOT NULL,
-    devoured BOOLEAN NOT NULL
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
 );
